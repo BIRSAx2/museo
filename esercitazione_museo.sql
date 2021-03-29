a4001a730d64f05d32c8ed750de3380c3a293f64
Mouhieddine Sabir <Mouhieddinesabir@gmail.com>
2021-03-29 12:03:34 +0200

    Update esercitazione_museo.sql

--- esercitazione_museo.sql
+++ esercitazione_museo.sql
@@ -44,7 +44,6 @@ create table biglietto
     data_validita date NOT NULL,
     categoria     int,
     evento        int  NOT NULL,
-
     primary key (numero),
     foreign key (categoria) references categoria (id) on update cascade,
     foreign key (evento) references evento (id) on update cascade
@@ -110,23 +109,23 @@ create table acquisto
 
 ## INSERT
 ## INSERT
-insert into evento
+insert into evento ( titolo, data_inizio, data_fine, numero_biglietti, tariffa, tipo)
 values ('Stravaganza degli anni 80', '2021-01-14', '2021-05-21', 200, 10.5, 'VISITA'),
 	   ('Le avanguardie', '2021-03-21', '2021-08-26', 250, 13.5, 'VISITA'),
-	   ("L'evoluzione del menswear", '2020-09-06', '2021-01-15', 175, 12.5, 'VISITA'),
+	   ('L\'evoluzione del menswear', '2020-09-06', '2021-01-15', 175, 12.5, 'VISITA'),
 	   ('100 icone', '2020-08-14', '2021-02-25', 300, 15, 'ESPOSIZIONE'),
 	   ('Martin Margiela - un rivoluzionario', '2020-11-24', '2021-04-25', 200, 12.5, 'ESPOSIZIONE'),
-	   ("Le influenze dell'Hip Hop", '2020-11-07', '2021-05-23', 200, 10.5, 'VISITA'),
+	   ('Le influenze dell\'Hip Hop', '2020-11-07', '2021-05-23', 200, 10.5, 'VISITA'),
 	   ('Una sfilata nel passato', '2020-03-24', '2020-09-05', 250, 13, 'ESPOSIZIONE');
-	   
-	   
-insert into categoria
+
+
+insert into categoria (descrizione, tipo_documento, percentuale_sconto)
 values ('Bambino (-12)', 'Carta Identità', 20),
 	   ('Anziano (+65)', 'Carta Identità', 25),
 	   ('Studente', 'Carta Universitaria', 15);
-	   
-	   
-insert into biglietto
+
+
+insert into biglietto (data_validita, categoria, evento)
 values ('2021-04-23', 2, 1),
 	   ('2021-06-14', 3, 2),
 	   ('2021-01-15', 3, 4),
@@ -143,9 +142,9 @@ values ('2021-04-23', 2, 1),
 	   ('2021-03-17', 2, 1),
 	   ('2020-12-16', NULL, 3),
 	   ('2020-12-16', NULL, 3);
-	   
-	 
-insert into articolo_aggiuntivo
+
+
+insert into articolo_aggiuntivo (costo, descrizione, tipo)
 values (30, 'Cappello New Era Logo', 'ACCESSORIO'),
 	   (7.5, 'Tote Bag', 'ACCESSORIO'),
 	   (24, 'Maglietta Logo', 'ACCESSORIO'),
@@ -154,9 +153,9 @@ values (30, 'Cappello New Era Logo', 'ACCESSORIO'),
 	   (30, 'Visita Guidata', 'SERVIZIO'),
 	   (10, 'Visita Guidata Cuffia', 'SERVIZIO'),
 	   (12, 'Visita Guidata Cuffia Multilingua', 'SERVIZIO');
-	   
-	   
-insert into associazione
+
+
+insert into associazione (numero, articolo_aggiuntivo)
 values (1, 1),
 	   (3, 2),
 	   (9, 6),
@@ -169,9 +168,9 @@ values (1, 1),
 	   (4, 1),
 	   (4, 5),
 	   (4, 2);
-	   
 
-insert into utente
+
+insert into utente (email, password, nome, cognome, telefono, via, cap, numero_civico, citta, sigla_provincia)
 values ('nico53@example.org', '$2b$12$02dW.34ymSC2cfDydkrYre4J6WwvdolQQCGvWn5qqDq4UmuaLoTQu', 'Sebastiano', 'Pulci',
         '+39285810810', 'Incrocio Ferdinando', '16527', '9', 'Settimo Loretta sardo', 'PT'),
        ('matteotticesare@example.net', '$2b$12$DovV.TkdJZAr1i32HoeyFu/1dkWAQ4s/NCAE9gwdz2Uw0NUnd5hI.', 'Adamo',
@@ -191,12 +190,12 @@ values ('nico53@example.org', '$2b$12$02dW.34ymSC2cfDydkrYre4J6WwvdolQQCGvWn5qqD
        ('fornaciaricristina@example.com', '$2b$12$ci/UGkeRVzCxwUtYaTHuUeGpuwO23davY0zEzUFbSK0k7dwmHAMFa', 'Gianni',
         'Ficino', '+39117701354', 'Strada Tina', '50386', '858', 'Salvi veneto', 'BT'),
        ('dbenussi@example.net', '$2b$12$vZF4PAZWD1IueIcITL6W0eHSbxes82AP1kxPYU9ZJhx2f87irwMyW', 'Enrico', 'Calvo',
-        '+39846760143', 'Borgo Gino', '28179', '02', 'Bembo ligure', 'FR');
+        '+39846760143', 'Borgo Gino', '28179', '02', 'Bembo ligure', 'FR'),
        ('gennaro76@example.org', '$2b$12$qkSLMw7cNjyEqgYInRwbcGjGQLcfGksrPU4C2kyrDbrOkrJbKzMt9', 'Riccardo', 'Rossi',
-        '+3978926297', 'Marco Polo', '14897', '2', 'Cissone', 'CN'), 
+        '+3978926297', 'Marco Polo', '14897', '2', 'Cissone', 'CN');
 
 
-insert into pagamento
+insert into pagamento (id_transazione, importo,data)
 values ('df47973f-8cb7-11eb-95a6-c8b29b8908ae', 73.72, '2020-11-18 16:15:48'),
        ('df47e5a7-8cb7-11eb-bd36-c8b29b8908ae', 37.87, '2021-02-15 15:33:05'),
        ('df47e5a8-8cb7-11eb-900e-c8b29b8908ae', 122.25, '2021-01-14 12:23:56'),
@@ -208,10 +207,10 @@ values ('df47973f-8cb7-11eb-95a6-c8b29b8908ae', 73.72, '2020-11-18 16:15:48'),
        ('df480c02-8cb7-11eb-9975-c8b29b8908ae', 12.5, '2021-01-24 04:34:28'),
        ('df480c03-8cb7-11eb-8fb4-c8b29b8908ae', 26.77, '2021-02-23 09:00:31'),
        ('df480c03-8cb7-11eb-u6c9-c8b29b8908ae', 25, '2020-11-23 09:00:31');
-       
-       
-insert into acquisto
-values ('nico53@example.org', 'df47973f-8cb7-11eb-95a6-c8b29b8908ae', 2),   
+
+
+insert into acquisto (utente, pagamento, biglietto)
+values ('nico53@example.org', 'df47973f-8cb7-11eb-95a6-c8b29b8908ae', 2),
 	   ('nico53@example.org', 'df47973f-8cb7-11eb-95a6-c8b29b8908ae', 3),
 	   ('matteotticesare@example.net', 'df47e5a7-8cb7-11eb-bd36-c8b29b8908ae', 1),
 	   ('guido38@example.com', 'df47e5a8-8cb7-11eb-900e-c8b29b8908ae', 4),
@@ -226,10 +225,9 @@ values ('nico53@example.org', 'df47973f-8cb7-11eb-95a6-c8b29b8908ae', 2),
 	   ('dbenussi@example.net', 'df480c03-8cb7-11eb-8fb4-c8b29b8908ae', 13),
 	   ('dbenussi@example.net', 'df480c03-8cb7-11eb-8fb4-c8b29b8908ae', 14),
 	   ('gennaro76@example.org', 'df480c03-8cb7-11eb-u6c9-c8b29b8908ae', 15),
-	   ('gennaro76@example.org', 'df480c03-8cb7-11eb-u6c9-c8b29b8908ae', 16),
-                                   
-                                   
-                                   
+	   ('gennaro76@example.org', 'df480c03-8cb7-11eb-u6c9-c8b29b8908ae', 16);
+
+
 # I titoli e le date delle esposizioni tematiche che sono state tenute dal 1 gennaio al 31 dicembre di un determinato anno.
 
 select titolo, data_inizio, data_fine
